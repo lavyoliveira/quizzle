918a98033289679fb42e845f6c3b0b572d3735ff
Wendel Nogueira <43501645+wendel-nogueira@users.noreply.github.com>
2023-05-18 01:31:58 -0300

    FEAT: Add popup and sound feedback

--- lib/components/right.dart
+++ lib/components/right.dart
@@ -1,11 +1,139 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:quizzle/constants.dart';
 
-class RightAnswer extends StatelessWidget {
-  const RightAnswer({super.key});
+class RightAnswer {
+  static Future<void> alert(BuildContext context) async {
+    return showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          //title: const Text("Resposta errada"),
+          titlePadding: const EdgeInsets.all(0),
+          content: SizedBox(
+            height: 170,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.check_outlined,
+                  color: Colors.green,
+                  size: 50,
+                ),
+                const SizedBox(height: 12),
+                const Text(
+                  "Resposta correta",
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w600,
+                      color: kBlackColor),
+                ),
+                const SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Você acertou a resposta!",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w400,
+                          color: kBlackColor),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 12),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Você ganhou ",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w400,
+                            color: kBlackColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "10 pontos",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w400,
+                              color: bgColorBlue),
+                        ),
+                        Text(
+                          "!",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.w400,
+                              color: kBlackColor),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          actionsAlignment: MainAxisAlignment.center,
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blue,
+              ),
+              child: const Text(
+                "Ok",
+                style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w400,
+                    color: bgColorBlue),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  static Future<void> alertSound(BuildContext context) async {
+    FlutterSoundPlayer mPlayer = FlutterSoundPlayer();
+    Uint8List? soundData;
+
+    var asset = await rootBundle.load('assets/sounds/win.wav');
+    await mPlayer.openPlayer();
+    soundData = asset.buffer.asUint8List();
+
+    await mPlayer.setVolume(1.0);
+    await mPlayer.startPlayerFromStream(
+      codec: Codec.pcm16,
+      numChannels: 1,
+      sampleRate: 78100,
+    );
+
+    await mPlayer.feedFromStream(soundData);
+    await mPlayer.stopPlayer();
   }
 }
